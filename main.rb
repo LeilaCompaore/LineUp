33ed0689bb4cbcf234c481f4d2ff4b45c7283e51
Leila Compaore <wendsomleila@gmail.com>
2017-01-12 02:58:45 -0500

    hello world done

---
+++ main.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+get '/' do
+  'Hello world'
+end
+get '/login' do
+  'login page'
+end
+get '/register' do
+  'register page'
+end
+get '/admin' do
+  'admin login'
+end
+get '/profile' do
+  'profile page'
+end
